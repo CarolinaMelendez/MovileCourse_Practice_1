10e0fc74269a783e8f31ec04d7b0cdedcfeb1227
CarolinaMelendez <venecia.melendez@ucb.edu.bo>
2023-03-09 11:50:58 -0400

    EX-3 Added ToolBar

--- app/src/main/java/com/example/practica1/BackgroundActivity.kt
+++ app/src/main/java/com/example/practica1/BackgroundActivity.kt
@@ -4,8 +4,9 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+// import android.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
-
+import androidx.appcompat.widget.Toolbar
 class BackgroundActivity : AppCompatActivity() {
     private val btnOrange: Button
         get() = findViewById(R.id.btnOrange)
@@ -13,10 +14,16 @@ class BackgroundActivity : AppCompatActivity() {
         get() = findViewById(R.id.btnBlue)
     private val layaoutPrincipal: ConstraintLayout
         get() = findViewById(R.id.layoutPrincipal)
+    private  val toolBar: Toolbar
+        get() = findViewById(R.id.toolbar)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_background)
+
+        supportActionBar.let{
+            setSupportActionBar(toolBar)
+        }
 
         btnOrange.setOnClickListener {
             layaoutPrincipal.setBackgroundColor(Color.MAGENTA)
